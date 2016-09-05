49e3891a94ae33fbdf899c3def47a09fa11df2cf
Thomas Toye <toye.thomas@gmail.com>
2016-09-05 22:45:08 +0200

    Add error handler spec

---
+++ test/ErrorHandlerSpec.scala
@@ -0,0 +1,30 @@
+import scala.concurrent.Future
+import org.scalatestplus.play._
+import play.api.libs.json.Json
+import play.api.mvc._
+import play.api.test._
+import play.api.test.Helpers._
+
+
+class ErrorHandlerSpec extends PlaySpec with Results {
+  "Error handler" should {
+    "return JSON on server error" in {
+      val handler = new ErrorHandler()
+      val header = FakeRequest()
+      await(handler.onServerError(header, new Exception())).body.contentType must be(Some("application/json"))
+    }
+
+    "return JSON on client error" in {
+      val handler = new ErrorHandler()
+      val header = FakeRequest()
+      await(handler.onClientError(header, 403)).body.contentType must be(Some("application/json"))
+    }
+
+    "return status code on client error" in {
+      val handler = new ErrorHandler()
+      val header = FakeRequest()
+      (contentAsJson(handler.onClientError(header, 403)) \ "statusCode").as[Int] must be(403)
+    }
+  }
+
+}
