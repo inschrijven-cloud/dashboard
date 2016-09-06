647db70a6c205b2af5aa9b9dd523a180f231ee65
Thomas Toye <toye.thomas@gmail.com>
2016-09-06 13:02:55 +0200

    Add API controller spec

---
+++ test/ApiControllerSpec.scala
@@ -0,0 +1,17 @@
+import be.thomastoye.speelsysteem.dashboard.controllers.api.ApiController
+
+import scala.concurrent.Future
+import org.scalatestplus.play._
+import play.api.libs.json.Json
+import play.api.mvc._
+import play.api.test._
+import play.api.test.Helpers._
+
+class ApiControllerSpec extends PlaySpec with Results {
+  "Generic API controller" should {
+    "return created" in {
+      val controller = new ApiController()
+      controller.created("aoeu-snth") mustBe Created(Json.obj("status" -> "created", "id" -> "aoeu-snth"))
+    }
+  }
+}
