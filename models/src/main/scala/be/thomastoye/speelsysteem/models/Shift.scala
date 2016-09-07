06f35fc16ccc95ba2f6199a56e3e12867b47e466
Thomas Toye <toye.thomas@gmail.com>
2016-09-07 22:58:23 +0200

    Use case objects instead of case classes for shift kinds
    No new instances of these needed anyway

--- models/src/main/scala/be/thomastoye/speelsysteem/models/Shift.scala
+++ models/src/main/scala/be/thomastoye/speelsysteem/models/Shift.scala
@@ -20,22 +20,22 @@ object Shift {
 
   /** Common shift kinds */
   object ShiftKind {
-    case class Early() extends ShiftKind { override val mnemonic  = "VRO"}
-    case class Morning() extends ShiftKind { override val mnemonic = "VM" }
-    case class Noon() extends ShiftKind { override val mnemonic = "MID" }
-    case class Afternoon() extends ShiftKind { override val mnemonic = "NM" }
-    case class Evening() extends ShiftKind { override val mnemonic = "AV" }
-    case class External() extends ShiftKind { override val mnemonic = "EXT" }
-    case class Crew() extends ShiftKind { override val mnemonic = "LEI" }
+    case object Early extends ShiftKind { override val mnemonic  = "VRO"}
+    case object Morning extends ShiftKind { override val mnemonic = "VM" }
+    case object Noon extends ShiftKind { override val mnemonic = "MID" }
+    case object Afternoon extends ShiftKind { override val mnemonic = "NM" }
+    case object Evening extends ShiftKind { override val mnemonic = "AV" }
+    case object External extends ShiftKind { override val mnemonic = "EXT" }
+    case object CrewActivity extends ShiftKind { override val mnemonic = "LEI" }
 
     def apply(mnemonic: String) = mnemonic match {
-      case "VRO" => Early()
-      case "VM"  => Morning()
-      case "MID" => Noon()
-      case "NM"  => Afternoon()
-      case "AV"  => Evening()
-      case "EXT" => External()
-      case "LEI" => Crew()
+      case "VRO" => Early
+      case "VM"  => Morning
+      case "MID" => Noon
+      case "NM"  => Afternoon
+      case "AV"  => Evening
+      case "EXT" => External
+      case "LEI" => CrewActivity
     }
   }
 }
