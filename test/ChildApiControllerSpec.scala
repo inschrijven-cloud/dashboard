0353d4182898ecbdc643835f922dde215c9ccc2b
Thomas Toye <toye.thomas@gmail.com>
2016-10-24 13:15:26 +0200

    Test ChildApiController#all

--- test/ChildApiControllerSpec.scala
+++ test/ChildApiControllerSpec.scala
@@ -2,7 +2,7 @@ import be.thomastoye.speelsysteem.dashboard.controllers.api.ChildApiController
 import be.thomastoye.speelsysteem.data.ChildRepository
 import be.thomastoye.speelsysteem.data.util.UuidService
 import be.thomastoye.speelsysteem.models.{Address, Child, ContactInfo}
-import be.thomastoye.speelsysteem.models.JsonFormats.childFormat
+import be.thomastoye.speelsysteem.models.JsonFormats._
 import be.thomastoye.speelsysteem.models.Shift.Id
 import org.scalatestplus.play.PlaySpec
 import play.api.mvc.Results
@@ -45,6 +45,44 @@ class ChildApiControllerSpec extends PlaySpec with Results {
       val controller = new ChildApiController(childRepo, uuidService)
       status(controller.getById("existing-id").apply(FakeRequest())) mustBe OK
       contentAsJson(controller.getById("existing-id").apply(FakeRequest())) mustBe Json.toJson(child)
+    }
+  }
+
+  "ChildApiController#all" should {
+    val child1 = Child("first", "last", Address.empty, ContactInfo.empty, None, Seq.empty)
+    val child2 = Child("first2", "last2", Address.empty, ContactInfo.empty, None, Seq.empty)
+
+    val uuidService = new UuidService {
+      override def random: String = ???
+    }
+
+
+    "return list of children in the database" in {
+      val childRepo = new ChildRepository {
+        override def addAttendancesForChild(id: Id, dayId: Id, shifts: Seq[Id]): Future[Option[Unit]] = ???
+        override def count: Future[Int] = ???
+        override def update(id: Id, child: Child): Future[Unit] = ???
+        override def insert(id: Id, child: Child): Future[Id] = ???
+        override def findById(id: Id): Future[Option[(Id, Child)]] = ???
+        override def findAll: Future[Seq[(Id, Child)]] = Future.successful(Seq(("first-id", child1), ("second-id", child2)))
+      }
+
+      val controller = new ChildApiController(childRepo, uuidService)
+      contentAsJson(controller.all.apply(FakeRequest())) mustBe Json.arr(Json.toJson(("first-id", child1)), Json.toJson(("second-id", child2)))
+    }
+
+    "return empty JSON list if there are no children in the database" in {
+      val childRepo = new ChildRepository {
+        override def addAttendancesForChild(id: Id, dayId: Id, shifts: Seq[Id]): Future[Option[Unit]] = ???
+        override def count: Future[Int] = ???
+        override def update(id: Id, child: Child): Future[Unit] = ???
+        override def insert(id: Id, child: Child): Future[Id] = ???
+        override def findById(id: Id): Future[Option[(Id, Child)]] = ???
+        override def findAll: Future[Seq[(Id, Child)]] = Future.successful(Seq.empty)
+      }
+
+      val controller = new ChildApiController(childRepo, uuidService)
+      contentAsJson(controller.all.apply(FakeRequest())) mustBe Json.arr()
     }
   }
 }
