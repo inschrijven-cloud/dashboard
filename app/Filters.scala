6600f899be051f9c7579af06ddea792b1b5a2577
Thomas Toye <thomas@toye.io>
2017-10-19 17:57:40 +0200

    Remove CORS filter
    Let nginx fronting handle this

--- app/Filters.scala
+++ app/Filters.scala
@@ -2,8 +2,7 @@ import javax.inject.Inject
 
 import play.api.http.HttpFilters
 import play.api.mvc.EssentialFilter
-import play.filters.cors.CORSFilter
 
-class Filters @Inject() (corsFilter: CORSFilter) extends HttpFilters {
-  def filters: Seq[EssentialFilter] = Seq(corsFilter)
+class Filters @Inject() () extends HttpFilters {
+  def filters: Seq[EssentialFilter] = Seq()
 }
