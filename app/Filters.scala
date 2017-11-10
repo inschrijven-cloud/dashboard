ff120183f88f5a819635ce0d399897c6f5c0b666
Thomas Toye <thomas@toye.io>
2017-11-10 22:52:11 +0100

    Revert "Remove CORS filter"
    This reverts commit 6600f899be051f9c7579af06ddea792b1b5a2577.

--- app/Filters.scala
+++
@@ -1,8 +0,0 @@
-import javax.inject.Inject
-
-import play.api.http.HttpFilters
-import play.api.mvc.EssentialFilter
-
-class Filters @Inject()() extends HttpFilters {
-  def filters: Seq[EssentialFilter] = Seq()
-}
